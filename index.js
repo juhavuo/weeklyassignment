8005295500ffa4468df211ccd6f0f74ce821dcbd
juhavuo <juha.vuokko@pp.inet.fi>
2019-03-27 14:53:55 +0200

    file is saved with proper name

--- index.js
+++ index.js
@@ -9,16 +9,30 @@ console.log(json_obj[0].time);
 
 const multer = require('multer');
 
-const upload = multer({dest: './uploads'});
+//https://stackoverflow.com/questions/32184589/renaming-an-uploaded-file-using-multer-doesnt-work-express-js
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    let extArray = file.mimetype.split("/");
+    let extension = extArray[1];
+    cb(null, file.fieldname + '-' + Date.now()+ '.' +extension)
+  }
+});
+
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
 
     res.send("hello world");
 });
 
-app.post('/upload_photo',upload.single('cat_test_image'),async (req, res) =>{
-  console.log(req);
-  res.send("OK");
+app.post('/upload_photo',upload.single('cat_test_image'), (req, res, next) =>{
+  const filename = req.file.filename;
+  console.log(filename);
+
+  res.send(req.file);
 });
 
 
