74db5986741060328b8c1367be7ce4c5eb5ada4a
juhavuo <juha.vuokko@pp.inet.fi>
2019-03-27 09:54:25 +0200

    starting post method to get the image

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ require('dotenv').config();
 const fs = require('fs');
 const json_obj = JSON.parse(fs.readFileSync('./jsons/cats.json','utf8'));
 console.log(json_obj[0].time);
+
+const multer = require('multer');
+
+const upload = multer({dest: './uploads'});
+
 app.get('/', (req, res) => {
 
     res.send("hello world");
 });
+
+app.post('/upload_photo',upload.single('cat_test_image'),async (req, res) =>{
+  console.log(req);
+  res.send("OK");
+});
+
+
 
 app.listen(process.env.APP_PORT);
